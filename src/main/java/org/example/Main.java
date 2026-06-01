bd21bcbb373106543b6819362a25050c47293b81
Mate Nozadze <143351472+MateNozadze@users.noreply.github.com>
2026-06-01 16:35:10 +0400

    Update output message in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("another user");
+        System.out.printf("another user for changes");
 
         System.out.println("pr hello changes");
         for (int i = 1; i <= 5; i++) {
